0f93a57bd1b7ca0637cd017dcdb0566af7dc57c7
mcoughlin2 <mike@metronome.com>
2022-12-01 12:46:58 -0600

    adding usage script

---
+++ scripts/add-plan-to-customer-with-price-adjustments/generate-usage-data/generate-usage-data.py
@@ -0,0 +1,56 @@
+from datetime import datetime, timedelta, timezone
+from pprint import pprint
+import random
+import uuid
+import json
+
+import requests
+
+token = '<API Token>'
+EnterpriseCustomer = 'customer_id'
+StandardCustomer = 'customer_id'
+CustomerIds = [EnterpriseCustomer, StandardCustomer]
+userId = []
+
+for _ in range(58):
+    userId.append(uuid.uuid4().hex)
+
+def ingest(events):
+    remainder = events
+    with open('ingest.json', 'w') as f:
+        json.dump(events, f)
+    f.close()
+    print('events sent successfully')
+
+def random_monthly_target(n):
+    return random.randint(n, 2*n)
+
+events = []
+for cid in CustomerIds: 
+    customerId = cid
+    t = datetime.now(timezone.utc) - timedelta(days=33)
+    while t < datetime.now(timezone.utc):
+        def append_event(event_type, properties):
+            events.append({
+                "transaction_id": uuid.uuid4().hex,
+                "timestamp": t.isoformat(),
+                "event_type": event_type,
+                "customer_id": customerId,
+                "properties": properties,
+            })
+        if cid == StandardCustomer:
+                append_event('gb_logs_ingested', {
+                    "gb_ingested":random_monthly_target(10000),
+                    "region": random.choice(["eu-west-1","us-east-1"]),
+                    "cloud": random.choice(["aws","gcp","azure"])
+                })
+        if cid == EnterpriseCustomer:
+                user_id = uuid.uuid1().fields[1] + random.randint(1,1005)
+                append_event('user_login', {
+                    "user_id": random.choice(userId),
+                    "user_level": random.choice(["admin","user","readOnly"])
+                })
+
+        t += timedelta(hours=1)
+
+ingest(events)
